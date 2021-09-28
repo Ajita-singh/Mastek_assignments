c58fe6cfc5aa0b1ca4cac0a255b2aecde06352ac
Ajita <34018203+Ajita-singh@users.noreply.github.com>
2021-09-28 15:22:24 +0530

    Delete dem11 directory

--- dem11/main - Copy.js
+++
@@ -1,9 +0,0 @@
-function getJsonData(){
-    const xmlhttp = new XMLHttpRequest();
-xmlhttp.onload = function() {
-  const person = JSON.parse(this.responseText);
-  document.getElementById("root").innerHTML = person.firstName+" "+person.lastName;
-}
-xmlhttp.open("GET", "person.json");
-xmlhttp.send();
-}
